cf54385abaeeff4919fe8563737f08c4c9446ce5
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-24 08:43:09 +0100

    first commit

---
+++ src/main/java/org/example/HO.java
@@ -0,0 +1,19 @@
+package org.example;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Vector;
+
+public class HO extends JFrame  {
+    public static void main(String[] args) {
+        try {
+            Receive receiver = new Receive("BO1");
+        } catch (Exception et) {
+            et.printStackTrace();
+        }
+        try {
+            Receive receiver = new Receive("BO2");
+        } catch (Exception et) {
+            et.printStackTrace();
+        }
+    }
+}
